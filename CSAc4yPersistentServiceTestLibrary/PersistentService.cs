1ce9a31840f90a952d23139b00953dbd1b7aa3c2
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-05 09:52:22 +0000

    Add project files.

---
+++ CSAc4yPersistentServiceTestLibrary/PersistentService.cs
@@ -0,0 +1,52 @@
+﻿using CSAc4y.Class;
+using CSAc4yObjectService.Class;
+using CSAc4yService.Class;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAc4yPersistentServiceTestLibrary
+{
+    public class PersistentService
+    {
+        private AllContext context { get; set; }
+        private Ac4yIdentificationBaseEntityMethods ac4YIdentificationBaseEntityMethods = new Ac4yIdentificationBaseEntityMethods(context);
+
+
+        public GetObjectResponse GetFirstByTemplate(int id)
+        {
+            var response = new GetObjectResponse();
+            try
+            {
+                response.setObject(ac4YIdentificationBaseEntityMethods.findFirstById(id));
+                response.Result = new Ac4yProcessResult() { Code = "1" };
+            }
+            catch (Exception exception)
+            {
+                response.Result = (new Ac4yProcessResult() { Code = "-1", Message = exception.Message });
+            }
+
+            return response;
+        }
+
+        public GetObjectResponse Save(Ac4yIdentificationBase ac4YIdentificationBase)
+        {
+            var response = new GetObjectResponse();
+            try
+            {
+                ac4YIdentificationBaseEntityMethods.addNewAc4yIdentificationBase(ac4YIdentificationBase);
+                response.Result = new Ac4yProcessResult() { Code = "1" };
+            }
+            catch (Exception exception)
+            {
+                response.Result = (new Ac4yProcessResult() { Code = "-1", Message = exception.Message });
+            }
+
+            return response;
+
+        }
+    }
+}
